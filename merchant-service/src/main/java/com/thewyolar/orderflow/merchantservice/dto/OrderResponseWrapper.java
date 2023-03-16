673cf7acee50a5cfd7d207300cf6faae2a81d517
thewyolar <thewyolar@gmail.com>
2023-03-16 21:34:02 +0300

    refactor: update order response wrapper model

--- merchant-service/src/main/java/com/thewyolar/orderflow/merchantservice/dto/OrderResponseWrapper.java
+++ merchant-service/src/main/java/com/thewyolar/orderflow/merchantservice/dto/OrderResponseWrapper.java
@@ -1,13 +1,24 @@
 package com.thewyolar.orderflow.merchantservice.dto;
 
+import com.thewyolar.orderflow.merchantservice.util.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 @Setter
-public class OrderResponseWrapper extends OrderResponseDTO {
+public class OrderResponseWrapper {
+    private UUID orderId;
+    private String name;
+    private double amount;
+    private String currency;
+    private UUID merchantId;
+    private OffsetDateTime dateCreate;
+    private OffsetDateTime dateUpdate;
+    private OrderStatus status;
     private List<TransactionResponseDTO> transactions;
 }
 
