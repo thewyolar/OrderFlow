7932bbd323558188782f8cf276a732239887a7a9
thewyolar <thewyolar@gmail.com>
2023-03-16 15:43:50 +0300

    feat: add model mapper bean and dependency

--- merchant-service/src/main/java/com/thewyolar/orderflow/merchantservice/MerchantServiceApplication.java
+++ merchant-service/src/main/java/com/thewyolar/orderflow/merchantservice/MerchantServiceApplication.java
@@ -1,13 +1,20 @@
 package com.thewyolar.orderflow.merchantservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MerchantServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MerchantServiceApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
