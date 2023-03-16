76649955cbbbfeffc2f27a02335b2d3175593d09
thewyolar <thewyolar@gmail.com>
2023-03-16 13:12:57 +0300

    Initial commit

---
+++ payform-service/src/test/java/com/thewyolar/orderflow/payformservice/PayformServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thewyolar.orderflow.payformservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayformServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
