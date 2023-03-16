b00ea4fde04d3f816bdb6228125cc27517fae09a
thewyolar <thewyolar@gmail.com>
2023-03-16 21:01:58 +0300

    refactor: add merchant controller

---
+++ merchant-service/src/main/java/com/thewyolar/orderflow/merchantservice/controller/MerchantController.java
@@ -0,0 +1,39 @@
+package com.thewyolar.orderflow.merchantservice.controller;
+
+import com.thewyolar.orderflow.merchantservice.dto.OrderDTO;
+import com.thewyolar.orderflow.merchantservice.dto.OrderResponseDTO;
+import com.thewyolar.orderflow.merchantservice.dto.OrderResponseWrapper;
+import com.thewyolar.orderflow.merchantservice.dto.TransactionResponseDTO;
+import com.thewyolar.orderflow.merchantservice.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/merchant")
+public class MerchantController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/add")
+    public ResponseEntity<OrderResponseDTO> addOrder(@RequestBody OrderDTO order) {
+        OrderResponseDTO orderResponseDTO = orderService.createOrder(order);
+        return ResponseEntity.ok(orderResponseDTO);
+    }
+
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<OrderResponseWrapper> getOrder(@PathVariable UUID orderId) throws NotFoundException {
+        OrderResponseWrapper orderResponseWrapper = orderService.getOrderById(orderId);
+        return ResponseEntity.ok(orderResponseWrapper);
+    }
+
+    @PostMapping("/order/{transactionId}/refund")
+    public ResponseEntity<TransactionResponseDTO> refundOrder(@PathVariable UUID transactionId) throws NotFoundException {
+        TransactionResponseDTO transactionResponseDTO = orderService.refundOrder(transactionId);
+        return ResponseEntity.ok(transactionResponseDTO);
+    }
+}
