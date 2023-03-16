c150e55442e0b70e4d79945f284af92519a8a4f8
thewyolar <thewyolar@gmail.com>
2023-03-16 13:40:21 +0300

    feat: add order and transaction models

---
+++ merchant-service/src/main/java/com/thewyolar/orderflow/merchantservice/model/Order.java
@@ -0,0 +1,51 @@
+package com.thewyolar.orderflow.merchantservice.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.thewyolar.orderflow.merchantservice.util.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID orderId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "amount")
+    private Double amount;
+
+    @Column(name = "currency")
+    private String currency;
+
+    @Column(name = "merchant_id")
+    private UUID merchantId;
+
+    @Column(name = "date_create")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private OffsetDateTime dateCreate;
+
+    @Column(name = "date_update")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private OffsetDateTime dateUpdate;
+
+    @Column(name = "expired_date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private OffsetDateTime expiredDate;
+
+    @Column(name = "orderStatus")
+    private OrderStatus status;
+
+    @OneToMany(mappedBy = "order", fetch = FetchType.LAZY)
+    private List<Transaction> transactions;
+}
---
+++ merchant-service/src/main/java/com/thewyolar/orderflow/merchantservice/model/Transaction.java
@@ -0,0 +1,43 @@
+package com.thewyolar.orderflow.merchantservice.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.thewyolar.orderflow.merchantservice.util.TransactionStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID transactionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Order order;
+
+    @Column(name = "merchant_id")
+    private UUID merchantId;
+
+    @Column(name = "date_create")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private LocalDateTime dateCreate;
+
+    @Column(name = "date_update")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private LocalDateTime dateUpdate;
+
+    @Column(name = "context")
+    private String context;
+
+    @Column(name = "status")
+    private TransactionStatus status;
+
+    @Column(name = "type")
+    private String type;
+}
