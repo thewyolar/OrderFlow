4a94e9849dbe2152690965748a5b7593dfab4ea2
thewyolar <thewyolar@gmail.com>
2023-03-16 14:00:34 +0300

    refactor: update transaction model

--- merchant-service/src/main/java/com/thewyolar/orderflow/merchantservice/model/Transaction.java
+++ merchant-service/src/main/java/com/thewyolar/orderflow/merchantservice/model/Transaction.java
@@ -24,6 +24,12 @@ public class Transaction {
     @Column(name = "merchant_id")
     private UUID merchantId;
 
+    @Column(name = "amount")
+    private Double amount;
+
+    @Column(name = "currency")
+    private String currency;
+
     @Column(name = "date_create")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
     private LocalDateTime dateCreate;
