556ef610a194f03db9f9f582dcb8216b755813fc
thewyolar <thewyolar@gmail.com>
2023-03-17 14:22:59 +0300

    feat: add spring validation, swagger support and update model mapper config

--- merchant-service/src/main/java/com/thewyolar/orderflow/merchantservice/MerchantServiceApplication.java
+++ merchant-service/src/main/java/com/thewyolar/orderflow/merchantservice/MerchantServiceApplication.java
@@ -1,9 +1,7 @@
 package com.thewyolar.orderflow.merchantservice;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MerchantServiceApplication {
@@ -11,10 +9,5 @@ public class MerchantServiceApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(MerchantServiceApplication.class, args);
-    }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
     }
 }
