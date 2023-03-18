c1b6b5649f0c85b3260cadd7af2a0ff17c9dcc72
thewyolar <thewyolar@gmail.com>
2023-03-19 00:00:31 +0300

    refactor: move spring doc config

--- merchant-service/src/main/java/com/thewyolar/orderflow/merchantservice/config/SpringDocConfig.java
+++ merchant-service/src/main/java/com/thewyolar/orderflow/merchantservice/config/SpringDocConfig.java
@@ -1,4 +1,4 @@
-package com.thewyolar.orderflow.merchantservice;
+package com.thewyolar.orderflow.merchantservice.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
