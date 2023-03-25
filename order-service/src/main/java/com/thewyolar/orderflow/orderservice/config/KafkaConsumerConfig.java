30c06de5c3f110cd8f17554be150327045e67abb
thewyolar <thewyolar@gmail.com>
2023-03-25 16:48:42 +0300

    refactor: kafka config divided on consumer and producer

--- order-service/src/main/java/com/thewyolar/orderflow/orderservice/config/KafkaConsumerConfig.java
+++ order-service/src/main/java/com/thewyolar/orderflow/orderservice/config/KafkaConsumerConfig.java
@@ -2,9 +2,7 @@ package com.thewyolar.orderflow.orderservice.config;
 
 import com.thewyolar.orderflow.orderservice.dto.PaymentResponseDTO;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.LongDeserializer;
-import org.apache.kafka.common.serialization.LongSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,39 +11,18 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @EnableKafka
 @Configuration
-public class KafkaConfig {
+public class KafkaConsumerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String kafkaServer;
 
     @Value("${spring.kafka.consumer.group-id}")
     private String kafkaGroupId;
-
-    @Bean
-    public Map<String, Object> producerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-
-        return props;
-    }
-
-    @Bean
-    public ProducerFactory<Long, PaymentResponseDTO> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(producerConfigs());
-    }
-
-    @Bean
-    public KafkaTemplate<Long, PaymentResponseDTO> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
 
     @Bean
     public Map<String, Object> consumerConfigs() {
