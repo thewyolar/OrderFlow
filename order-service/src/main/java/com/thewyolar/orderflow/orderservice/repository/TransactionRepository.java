1cdb1ec7847a0f4b83be1bfce39e496a2f72ea53
thewyolar <thewyolar@gmail.com>
2023-03-25 13:54:46 +0300

    feat: add method to find transactions by order and status for transaction repository

--- order-service/src/main/java/com/thewyolar/orderflow/orderservice/repository/TransactionRepository.java
+++ order-service/src/main/java/com/thewyolar/orderflow/orderservice/repository/TransactionRepository.java
@@ -1,11 +1,15 @@
 package com.thewyolar.orderflow.orderservice.repository;
 
+import com.thewyolar.orderflow.orderservice.model.Order;
 import com.thewyolar.orderflow.orderservice.model.Transaction;
+import com.thewyolar.orderflow.orderservice.util.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+    List<Transaction> findByOrderAndStatus(Order order, TransactionStatus transactionStatus);
 }
