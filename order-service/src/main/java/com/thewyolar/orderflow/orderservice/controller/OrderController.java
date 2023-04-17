75dc1f045ab0693d81b6443375cfb00e74c2269b
thewyolar <thewyolar@gmail.com>
2023-04-17 22:27:47 +0300

    feat: add order controller

---
+++ order-service/src/main/java/com/thewyolar/orderflow/orderservice/controller/OrderController.java
@@ -0,0 +1,44 @@
+package com.thewyolar.orderflow.orderservice.controller;
+
+import com.thewyolar.orderflow.orderservice.dto.OrderDTO;
+import com.thewyolar.orderflow.orderservice.dto.OrderResponseDTO;
+import com.thewyolar.orderflow.orderservice.dto.OrderResponseWrapper;
+import com.thewyolar.orderflow.orderservice.dto.TransactionResponseDTO;
+import com.thewyolar.orderflow.orderservice.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/merchant/orders")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/add")
+    public ResponseEntity<OrderResponseDTO> addOrder(@RequestBody OrderDTO order) {
+        OrderResponseDTO orderResponseDTO = orderService.createOrder(order);
+        return ResponseEntity.ok(orderResponseDTO);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponseWrapper> getOrder(@PathVariable UUID orderId) {
+        OrderResponseWrapper orderResponseWrapper = orderService.getOrderById(orderId);
+        return ResponseEntity.ok(orderResponseWrapper);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<String> deleteOrder(@PathVariable UUID orderId) {
+        orderService.deleteOrderById(orderId);
+        return ResponseEntity.ok("Order with id=" + orderId + " deleted successfully.");
+    }
+
+    @PostMapping("/{transactionId}/refund")
+    public ResponseEntity<TransactionResponseDTO> refundOrder(@PathVariable UUID transactionId) {
+        TransactionResponseDTO transactionResponseDTO = orderService.refundOrder(transactionId);
+        return ResponseEntity.ok(transactionResponseDTO);
+    }
+}
