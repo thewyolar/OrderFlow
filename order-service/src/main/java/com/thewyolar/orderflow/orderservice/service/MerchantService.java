2c3bbc6cbe30f72021496748678d6c8f466af473
thewyolar <thewyolar@gmail.com>
2023-04-17 22:21:29 +0300

    feat: add merchant service

---
+++ order-service/src/main/java/com/thewyolar/orderflow/orderservice/service/MerchantService.java
@@ -0,0 +1,42 @@
+package com.thewyolar.orderflow.orderservice.service;
+
+import com.thewyolar.orderflow.orderservice.dto.MerchantDTO;
+import com.thewyolar.orderflow.orderservice.model.Merchant;
+import com.thewyolar.orderflow.orderservice.repository.MerchantRepository;
+import com.thewyolar.orderflow.orderservice.service.mapper.MerchantMapper;
+import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
+
+import java.util.UUID;
+
+@Service
+public class MerchantService {
+    private final MerchantRepository merchantRepository;
+
+    private final MerchantMapper merchantMapper;
+
+    public MerchantService(MerchantRepository merchantRepository, MerchantMapper merchantMapper) {
+        this.merchantRepository = merchantRepository;
+        this.merchantMapper = merchantMapper;
+    }
+
+    public Merchant getMerchantById(UUID id) {
+        return merchantRepository.findById(id).orElseThrow(() -> new NotFoundException("Merchant not found"));
+    }
+
+    public Merchant createMerchant(MerchantDTO merchantDTO) {
+        Merchant merchant = merchantMapper.toMerchant(merchantDTO);
+        return merchantRepository.save(merchant);
+    }
+
+    public Merchant updateMerchant(UUID merchantId, MerchantDTO merchant) {
+        Merchant existingMerchant = merchantRepository.findById(merchantId).orElseThrow(() -> new NotFoundException("Merchant not found"));;
+        existingMerchant.setName(merchant.getName());
+        existingMerchant.setSiteUrl(merchant.getSiteUrl());
+        return merchantRepository.save(existingMerchant);
+    }
+
+    public void deleteMerchantById(UUID merchantId) {
+        merchantRepository.deleteById(merchantId);
+    }
+}
