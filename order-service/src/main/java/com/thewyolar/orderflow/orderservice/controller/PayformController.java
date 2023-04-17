b56a5450aca6a5681a03b2de77f9c6e2b3b5732c
thewyolar <thewyolar@gmail.com>
2023-04-17 22:23:50 +0300

    refactor: cleanup code

--- order-service/src/main/java/com/thewyolar/orderflow/orderservice/controller/PayformController.java
+++ order-service/src/main/java/com/thewyolar/orderflow/orderservice/controller/PayformController.java
@@ -8,10 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/payform")
 public class PayformController {
 
