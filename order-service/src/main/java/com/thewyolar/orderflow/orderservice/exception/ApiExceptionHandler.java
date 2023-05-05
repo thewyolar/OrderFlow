292e6988d88bda47692c9331ce65e33c4e6d2eea
thewyolar <thewyolar@gmail.com>
2023-05-06 01:43:52 +0300

    refactor: update api exception handler

--- order-service/src/main/java/com/thewyolar/orderflow/orderservice/exception/ApiExceptionHandler.java
+++ order-service/src/main/java/com/thewyolar/orderflow/orderservice/exception/ApiExceptionHandler.java
@@ -10,36 +10,36 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(MerchantNotFoundException.class)
-    protected ResponseEntity<Object> handleMerchantNotFound(MerchantNotFoundException ex) {
+    protected ResponseEntity<ApiError> handleMerchantNotFound(MerchantNotFoundException ex) {
         ApiError apiError = new ApiError(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND, ex.getMessage());
         return buildResponseEntity(apiError);
     }
 
     @ExceptionHandler(OrderNotFoundException.class)
-    protected ResponseEntity<Object> handleOrderNotFound(OrderNotFoundException ex) {
+    protected ResponseEntity<ApiError> handleOrderNotFound(OrderNotFoundException ex) {
         ApiError apiError = new ApiError(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND, ex.getMessage());
         return buildResponseEntity(apiError);
     }
 
     @ExceptionHandler(TransactionNotFoundException.class)
-    protected ResponseEntity<Object> handleTransactionNotFound(TransactionNotFoundException ex) {
+    protected ResponseEntity<ApiError> handleTransactionNotFound(TransactionNotFoundException ex) {
         ApiError apiError = new ApiError(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND, ex.getMessage());
         return buildResponseEntity(apiError);
     }
 
     @ExceptionHandler(CallbackException.class)
-    protected ResponseEntity<Object> handleCallback(CallbackException ex) {
+    protected ResponseEntity<ApiError> handleCallback(CallbackException ex) {
         ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
         return buildResponseEntity(apiError);
     }
 
     @ExceptionHandler(PaymentException.class)
-    protected ResponseEntity<Object> handlePayment(PaymentException ex) {
+    protected ResponseEntity<ApiError> handlePayment(PaymentException ex) {
         ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
         return buildResponseEntity(apiError);
     }
 
-    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+    private ResponseEntity<ApiError> buildResponseEntity(ApiError apiError) {
         return new ResponseEntity<>(apiError, apiError.getStatus());
     }
 }
